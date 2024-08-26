cfa4a66a20fe5a0a85340c2fdb16e38f839345f1
PrayonFernando <prayonfernando@gmail.com>
2024-08-27 05:19:59 +0530

    Service page

--- my-portfolio/app/page.jsx
+++ my-portfolio/app/page.jsx
@@ -1,13 +1,15 @@
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 import Social from "@/components/ui/Social";
+import Photo from "@/components/ui/Photo";
+import Stats from "@/components/ui/Stats";
 
 const Home = () => {
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Front-End Developer</span>
             <h1 className="h1">
               Hello I'm <br />{" "}
@@ -32,13 +34,20 @@ const Home = () => {
                 <span>Download CV</span>
                 <FiDownload className="text-xl" />
               </Button>
-              <div className="mb-8 xl:mb-0">Socials</div>
-              <Social />
+              <div className="mb-8 xl:mb-0">
+                <Social
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                />
+              </div>
             </div>
           </div>
-          <div></div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
         </div>
       </div>
+      <Stats />
     </section>
   );
 };
--- my-portfolio/components/ui/Social.jsx
+++ my-portfolio/components/ui/Social.jsx
@@ -1,5 +1,26 @@
-const Social = () => {
-  return <div>Social</div>;
+import Link from "next/link";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+
+const socials = [
+  { icons: <FaGithub />, path: "https://github.com/PrayonFernando" },
+  {
+    icons: <FaLinkedinIn />,
+    path: "https://www.linkedin.com/in/prayon-fernando-738b271b8/",
+  },
+];
+
+const Social = ({ containerStyles, iconStyles }) => {
+  return (
+    <div className={containerStyles}>
+      {socials.map((item, index) => {
+        return (
+          <Link key={index} href={item.path} className={iconStyles}>
+            {item.icons}
+          </Link>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Social;
